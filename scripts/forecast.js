1b7241543046842a264f65dcf3b605ec1b5646c0
Abhijeet07 <abhijeets809@gmail.com>
2020-01-04 20:53:40 +0530

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,42 @@
+const key  = 'mL3sPqnzrNtP3o9fcfYTGWDa6lPp7XG5';
+
+//city search
+//http://dataservice.accuweather.com/locations/v1/cities/search
+
+//current conditions
+//http://dataservice.accuweather.com/currentconditions/v1/{locationKey}
+
+//get weather info
+
+const getWeather = async (id) =>{
+
+        const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        const query  = `${id}?apikey=${key}`;
+        const response = await fetch(base + query);
+
+        const data = await response.json();
+
+        return data[0];
+}
+
+
+
+//get city information 
+const getCity = async (city) => {
+
+    const base  = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = `?apikey=${key}&q=${city}`;
+ 
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('manchester').then(data => {
+//       return getWeather(data.Key)
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(err => console.log(err));
+
